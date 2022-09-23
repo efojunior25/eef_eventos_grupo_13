da4667c2cd3cc95c02fb1e7efec2d728e1743685
Edson Junior <efojunior25@gmail.com>
2022-09-23 11:50:42 -0300

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,63 +1,37 @@
-const teste1 = document.getElementsByTagName("body")
-teste1.onLoad = listEvent()
+// const teste1 = document.getElementsByTagName("body")
+// teste1.onLoad = listEvent()
 
-async function listEvent() {
+async function listEvent(event) {
     try {
-        const body = querySelector("body")
-        const response = document.querySelector("https://xp41-soundgarden-api.herokuapp.com/events")
-        const listEvent = response.json()
-
-        listEvent.forEach((event) => {
-            const html = 
-            `<tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>05/03/2022 20:00</td>
-                  <td>Festival Coala${event.name}</td>
-                  <td>Miley Cyrus, Liniker, Smashing Pumpkins</td>
-                  <td>
-                    <a href="reservas.html" class="btn btn-dark"
-                      >ver reservas</a
-                    >
-                    <a href="editar.html" class="btn btn-secondary">editar</a>
-                    <a href="editar.html" class="btn btn-danger">excluir</a>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>05/03/2022 20:00</td>
-                  <td>Indie Fest</td>
-                  <td>Arctic Monkeys, The Kooks, Hiatus Kaiyote</td>
-                  <td>
-                    <a href="reservas.html" class="btn btn-dark"
-                      >ver reservas</a
-                    >
-                    <a href="editar.html" class="btn btn-secondary">editar</a>
-                    <a href="editar.html" class="btn btn-danger">excluir</a>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>05/03/2022 20:00</td>
-                  <td>Bourbon Jazz Festival</td>
-                  <td>Esperanza Spalding, Zimbo Trio, Serial Funkers</td>
-                  <td>
-                    <a href="reservas.html" class="btn btn-dark"
-                      >ver reservas</a
-                    >
-                    <a href="editar.html" class="btn btn-secondary">editar</a>
-                    <a href="editar.html" class="btn btn-danger">excluir</a>
-                  </td>
-                </tr>
-              </tbody>
-            `;
-        body.innerHTML += html
+        const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: 'follow'       
 
         });
+        const data = await response.json();
+
+        const eventoBody = document.querySelector('tbody')
+
+        data.forEach((event) => {
+        const html = `<tr>  <th scope="row">1</th>
+                            <td>${event.scheduled}</td>
+                            <td>${event.name}</td>
+                            <td>${event.attractions}</td>
+                            <td>
+                                <a href="reservas.html" class="btn btn-dark">ver reservas</a>
+                                <a href="editar-evento.html?id=${event._id}" class="btn btn-secondary">editar</a>
+                                <a href="excluir-evento.html?id=${event._id}" class="btn btn-danger">excluir</a>
+                            </td></tr>`;
+
+        eventoBody.innerHTML += html
+        });
+
     } catch (error) {
         console.log(error)
     }
 }
-
+listEvent()
 
 
